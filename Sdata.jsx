09e0ce43748055393092fde8686c5590cb009241
hrushikesh9899 <84874199+hrushikesh9899@users.noreply.github.com>
2021-06-11 18:54:32 +0530

    final

---
+++ Sdata.jsx
@@ -0,0 +1,39 @@
+import ro from "./Images/ronaldo.jpg";
+import me from "./Images/messi.jpg";
+import ne from "./Images/neymar.jpg";
+import ke from "./Images/kevin.jpg";
+import de from "./Images/dybala.jpg";
+const Sdata =  [
+    {
+        sname : "Ronaldo",
+        ronaldo : {ro},
+        title : "players",
+        links : "https://www.pinterest.com/pin/774056254699619866/"
+    },
+    {
+        sname : "Messi",
+        messi : {me},
+        title : "players",
+        links : "https://www.pinterest.com/search/pins/?rs=ac&len=2&q=messi%20wallpaper%20full%20hd%204k&eq=messi&etslf=6896&term_meta[]=messi%7Cautocomplete%7C2&term_meta[]=wallpaper%7Cautocomplete%7C2&term_meta[]=full%7Cautocomplete%7C2&term_meta[]=hd%7Cautocomplete%7C2&term_meta[]=4k%7Cautocomplete%7C2"
+    },
+    {
+        sname : "kevin",
+        kevin : {ke},
+        title : "players",
+        links : "https://www.pinterest.com/search/pins/?q=kevin%20de%20bruyne%20pics&rs=typed&term_meta[]=kevin%7Ctyped&term_meta[]=de%7Ctyped&term_meta[]=bruyne%7Ctyped&term_meta[]=pics%7Ctyped"
+    },
+    {
+        sname : "neymar",
+        neymar : {ne},
+        title : "players",
+        links : "https://www.pinterest.com/search/pins/?q=neymar%20pics&rs=typed&term_meta[]=neymar%7Ctyped&term_meta[]=pics%7Ctyped"
+    },
+    {
+        sname : "dybala",
+        dybala : {de},
+        title : "players",
+        links : "https://www.google.com/search?source=univ&tbm=isch&q=dybala+hd+pics&sa=X&ved=2ahUKEwjLtMGbzI_xAhVTILcAHeZLCeUQjJkEegQIBxAC"
+    }
+]
+export default Sdata;
+export {ro,me,ne,ke,de};
